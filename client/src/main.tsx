31c12d9284b7f5c221782ee0f1314047f6825bca
Retchoid <bobbyjackets.one@gmail.com>
2026-08-19 21:04:42 -0400

    Load continuous Level 1 sky prototype

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -34,6 +34,7 @@ import "./final-regression-fix-pass.css";
 import "./strict-visual-repair.css";
 import "./visual-recovery-site.css";
 import "./level1-final-transition-calibration.css";
+import "./level1-continuous-sky-lighting.css";
 import "./level1-transition-sync";
 
 
